7907edf785cac2a35efc7ab290724dd836084f25
Petar Ploshtakov <p.a.ploshtakov@gmail.com>
2015-12-17 17:40:48 +0200

    main

--- TestGraci/src/Cinema.java
+++ TestGraci/src/Cinema.java
@@ -2,6 +2,14 @@
 public class Cinema {
 
 	public static void main(String[] args) {
+		Actor[] actors = new Actor[3];
+		actors[0] = new Actor("Daysy", "Ridley", "female", 23);
+		actors[1] = new Actor("Oscar", "Isaac", "male", 35);
+		actors[2] = new Actor("Mark", "Hamill", "male", 64);
+		
+		
+		
+		
 		// TODO Auto-generated method stub
 
 	}
